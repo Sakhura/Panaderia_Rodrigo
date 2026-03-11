45ebdf68b1f3afe81b1f39a3ddc289243fb97dce
Sakhura <sabina.romero@gmail.com>
2026-03-11 19:48:26 -0300

    producto

--- src/main/java/com/panaderia/rodrigo/model/Cliente.java
+++ src/main/java/com/panaderia/rodrigo/model/Cliente.java
@@ -1,4 +1,27 @@
 package com.panaderia.rodrigo.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nombre;
+    private String email;
+    private Double telefono;
+    private Integer direccion;
 }
--- src/main/java/com/panaderia/rodrigo/repository/ProductoRepository.java
+++ src/main/java/com/panaderia/rodrigo/repository/ProductoRepository.java
@@ -1,4 +1,32 @@
 package com.panaderia.rodrigo.repository;
 
+import com.panaderia.rodrigo.model.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductoRepository {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
+    }
+
+    public void save(Producto producto) {
+        productoRepository.save(producto);
+    }
+
+    public Producto findById(int id) {
+        return productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+    }
+
+    public void delete(int id) {
+        productoRepository.deleteById(id);
+    }
+
 }
